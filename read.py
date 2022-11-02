2e9d548555c43c79ec6ad76e40ef798afa828e49
keskhanal <keskhanal2413@gmail.com>
2022-11-02 20:12:03 +0545

    read images and videos

--- read.py
+++ read.py
@@ -1,7 +1,21 @@
 import cv2 as cv
 
-img = cv.imread('images/space.jpg')
+#reading images
+# img = cv.imread('images/space.jpg')
+# cv.imshow('space', img)
 
-cv.imshow('space', img)
+#cv.waitKey(0)
 
-cv.waitKey(0)
+
+#reading videos
+capture = cv.VideoCapture('videos/Sand.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('sand in a hand',frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
